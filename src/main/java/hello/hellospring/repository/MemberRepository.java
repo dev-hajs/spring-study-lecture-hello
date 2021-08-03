6fba8a7891275d818d7b711ca6ba0f4384f88aa8
하지수 <dev.hajs@gmail.com>
2021-08-03 23:07:51 +0900

    회원 서비스 개발

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -2,7 +2,7 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
@@ -10,5 +10,5 @@ public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
-    Collection<Member> findAll();
+    List<Member> findAll();
 }
