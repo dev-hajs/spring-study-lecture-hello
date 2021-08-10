97084ad1236228edb5f7384a31d640d3e7e55845
하지수 <dev.hajs@gmail.com>
2021-08-10 23:32:05 +0900

    AOP 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -21,6 +21,11 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+//    @Bean // AOP 를 spring bean 으로 등록시키기 위해서 config 로 명시적 선언하는게 더 좋지만, 예제에서는 component annotation 을 사용
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
